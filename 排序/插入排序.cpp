3e412b34cb7b6721f794703be2ab7b2a265dcd23
jiaqiangwjq <jiaqiangwjq@gmail.com>
2019-08-31 23:51:42 +0800

    add 插入排序.cpp

---
+++ 排序/插入排序.cpp
@@ -0,0 +1,14 @@
+/* 插入排序 InsertSort */
+
+void insertSort(int A[], int n){
+	int i, j;
+	for(i = 1; i < n; ++i){		/* 从第二个元素开始，第一个元素看作一个有序序列 */
+		int temp = A[i];
+		j = i;
+		while(j > 1 && temp < A[j-1]){		/* 找到第一个比 temp 大的元素 */
+			A[j] = A[j-1];		/* 比 temp 小的元素都后移 */
+			j--;
+		}
+		A[j] = temp;	/* 插入位置 */
+	}
+}
