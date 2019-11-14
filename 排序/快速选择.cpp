92a3f31529a171f365dcf0fd4de218548ba5cbd3
jiaqiangwjq <jiaqiangwjq@gmail.com>
2019-11-14 12:59:03 +0800

    add 快速选择.cpp

---
+++ 排序/快速选择.cpp
@@ -0,0 +1,45 @@
+/* 快速选择算法 */
+
+/*
+* 利用快速排序的思想，但不需要对所有元素都进行排序。
+* 一般用来求解第 K 个最小/大元素的问题。
+*/
+
+int findKthLargest(int a[], int k, int n){		/* n 为元素个数, k 为待求参数 */
+	k = n - k;
+	int left = 0, right = n - 1;
+	while(left < right){
+		int j = partition(a, left, right);
+		if(j == k)
+			break;
+		else if(j < k)
+			left = j + 1;
+		else
+			right = j - 1;
+	}
+	return a[k];
+}
+
+/* 快速排序的 partition 部分 */
+int partition(int a[], int left, int right){
+	int i, j, t, temp;
+	if(left > right)
+		return;
+	temp = a[left];
+	i = left;
+	j = right;
+	while(i != j){
+		while(a[j] >= temp && i < j)
+			j--;
+		while(a[i] <= temp && i < j)
+			i++;
+		if(i < j){
+			t = a[i];
+			a[i] = a[j];
+			a[j] = t;
+		}	
+	}
+	a[left] = a[i];
+	a[i] = temp;
+	return i;
+}
