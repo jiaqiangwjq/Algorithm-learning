65f8281dee1187ba2cc07f1a0abda6456d563ed3
jiaqiangwjq <jiaqiangwjq@gmail.com>
2020-04-20 16:55:44 +0800

    update quickSelect

--- 排序/快速选择.cpp
+++ 排序/快速选择.cpp
@@ -5,9 +5,16 @@
 * 一般用来求解第 K 个最小/大元素的问题。
 */
 
+#include<cstdio>
+#include<algorithm>
+using namespace std;
+
 int findKthLargest(int a[], int k, int n){		/* n 为元素个数, k 为待求参数 */
+	
 	k = n - k;
+	
 	int left = 0, right = n - 1;
+	
 	while(left < right){
 		int j = partition(a, left, right);
 		if(j == k)
@@ -17,29 +24,35 @@ int findKthLargest(int a[], int k, int n){		/* n 为元素个数, k 为待求参
 		else
 			right = j - 1;
 	}
+	
 	return a[k];
 }
 
 /* 快速排序的 partition 部分 */
 int partition(int a[], int left, int right){
+	
 	int i, j, t, temp;
+	
 	if(left > right)
 		return;
+	
 	temp = a[left];
 	i = left;
 	j = right;
+	
 	while(i != j){
 		while(a[j] >= temp && i < j)
 			j--;
+
 		while(a[i] <= temp && i < j)
 			i++;
-		if(i < j){
-			t = a[i];
-			a[i] = a[j];
-			a[j] = t;
-		}	
+		
+		if(i < j)
+			swap(a[i], a[j]);
 	}
+	
 	a[left] = a[i];
 	a[i] = temp;
+	
 	return i;
 }
