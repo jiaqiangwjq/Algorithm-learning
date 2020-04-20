c3b1cf40b3d028923582212c253eaa4b7f5a3537
jiaqiangwjq <jiaqiangwjq@gmail.com>
2020-04-20 13:40:44 +0800

    update quickSort function

--- 排序/快速排序.cpp
+++ 排序/快速排序.cpp
@@ -11,37 +11,32 @@ int a[101], n;  //定义两个全局变量，需要在子函数中使用它们
 void QuickSort(int left, int right)
 {
     int i, j, t, temp;
-    if(left > right)
-        return;
 
     //temp 中存放的就是基准数    
     temp = a[left];
 
     i = left;
     j = right;
-    while(i != j)
+	 
+    while(i != j)		//当哨兵 i 和哨兵 j 没有相遇时，交换两个数的位置
     {   
-        //顺序很重要,要先从右往左找，保证最后与 temp 交换的值一定比它小
-		//如果先从左向右找，那么最后与 temp 交换的值就比 temp 大了
+        /* 
+		 * 顺序很重要,要先从右往左找，保证最后与 temp 交换的值一定比它小
+		 * 如果先从左向右找，那么最后与 temp 交换的值就比 temp 大了
+		 */
         while(a[j] >= temp && i < j)
             j--;
+		a[left] = a[right];
         
         //再从左往右找
         while(a[i] <= temp && i < j)
             i++;
-
-        //当哨兵 i 和哨兵 j 没有相遇时，交换两个数的位置
-        if(i < j)
-        {
-            t = a[i];
-            a[i] = a[j];
-            a[j] = t;
-        }
+		a[right] = a[left];
+       
     }
 
     //将基准数归位
-    a[left] = a[i];
-    a[i] = temp;
+    a[left] = temp;
 
     QuickSort(left, i-1);   //递归处理 左 半部分
     QuickSort(i+1, right);  //递归处理 右 半部分
