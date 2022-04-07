cf83bd9349e4e84ae6340e2dc32778d67f574c45
jiaqiangwjq <jiaqiangwjq@gmail.com>
2022-04-07 16:35:42 +0800

    update leetcode 217-easy

--- leetcode/easy/217.cpp
+++ leetcode/easy/217.cpp
@@ -12,3 +12,18 @@ public:
         return false;
     }
 };
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
+
+        for (int x : nums) {
+            if (s.find(x) != s.end()) {
+                return true;
+            }
+            s.insert(x);
+        }
+        return false;
+    }
+};
