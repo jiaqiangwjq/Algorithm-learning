0a4041d578926cbac4a5cccf0f00b5919a7ec813
jiaqiangwjq <jiaqiangwjq@gmail.com>
2022-04-20 15:19:09 +0800

    leetcode 98-medium

---
+++ leetcode/medium/98.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    vector<int> ret;
+
+    void inOrder(TreeNode* root) {
+        if (root) {
+            inOrder(root->left);
+            ret.push_back(root->val);
+            inOrder(root->right);
+        }
+    }
+
+    bool isValidBST(TreeNode* root) {
+        if (!root) return true;
+        inOrder(root);
+
+        for (int i = 1; i < ret.size(); ++i) {
+            if (ret[i] <= ret[i-1]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
